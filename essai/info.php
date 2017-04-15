7f731858b2b121b641cdb24d4f7e015a7cbeef43
devops <devops@gosseyn3.home>
2017-04-15 22:08:28 +0200

    test

--- essai/info.php
+++ essai/info.php
@@ -8,7 +8,7 @@ echo date("l") . "<br>";
 // Prints the day, date, month, year, time, AM or PM !!!
 echo date("l jS \of F Y h:i:s A") . "<br>";
 
-// Prints October 3, 1975 was on a Friday
+// Prints October 3, 1975 was on a Friday !!!
 echo "Oct 3,1975 was on a ".date("l", mktime(0,0,0,10,3,1975)) . "<br>";
 
 // Use a constant in the format parameter
