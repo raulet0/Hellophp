f521b41b7895612d5fae2caee23fcb65ec8979bb
Ubuntu <carlos@TestMRA3.zuqfcvhlrhau5ldbyagr0nwihc.ax.internal.cloudapp.net>
2020-12-30 21:55:15 +0000

    TEST

--- essai/phpinfo.php
+++
@@ -1,9 +0,0 @@
-<h1>Hello PHP info !!!---???</h1>
-<?php
-    phpinfo();
-    // test from PC 20170418 14h11
-    // test from gosseyn3 20170418 22h20
-    // test from gosseyn3 20170702 16h40
-    // test from gosseyn3 20170703 21h00
-    // test from PC 20170717 16h05
-?>
